8a7ba435c260b7fcac36321a6cf8ebb594a44992
unknown <>
2017-07-21 12:21:02 +0800

    first commit

---
+++ prod.server.js
@@ -0,0 +1,62 @@
+var express = require('express')
+var compression = require('compression')
+var config = require('./config/index')
+var axios = require('axios')
+
+var port = process.env.PORT || config.build.port
+
+var app = express()
+
+var apiRoutes = express.Router()
+
+apiRoutes.get('/getDiscList', (req, res) => {
+  var url = 'https://c.y.qq.com/splcloud/fcgi-bin/fcg_get_diss_by_tag.fcg'
+  axios.get(url, {
+    headers: {
+      referer: 'https://y.qq.com/',
+      host: 'y.qq.com'
+    },
+    params: req.query
+  }).then((response) => {
+    res.json(response.data)
+  }).catch((e) => {
+    console.log(e)
+  })
+})
+apiRoutes.get('/lyric', (req, res) => {
+  var url = 'https://c.y.qq.com/lyric/fcgi-bin/fcg_query_lyric_new.fcg'
+  axios.get(url, {
+    headers: {
+      referer: 'https://y.qq.com/',
+      host: 'y.qq.com'
+    },
+    params: req.query
+  }).then((response) => {
+    var ret = response.data
+    if (typeof ret === 'string') {
+      var reg = /^\w+\(({[^()]+})\)$/
+      var mathes = ret.match(reg)
+      if (mathes) {
+        ret = JSON.parse(mathes[1])
+      }
+    }
+    res.json(ret)
+  }).catch((e) => {
+    console.log(e)
+  })
+})
+
+app.use('/api', apiRoutes)
+
+// 压缩所有响应
+app.use(compression())
+
+app.use(express.static('./dist'))
+
+module.exports = app.listen(port, function (err) {
+  if (err) {
+    console.log(err)
+    return
+  }
+  console.log('listening at http://localhost:' + port + '\n')
+})
---
+++ src/common/js/config.js
@@ -0,0 +1,8 @@
+/**
+ * Created by tianzhihong on 2017/7/4.
+ */
+export const playMode = {
+  sequence: 0,
+  loop: 1,
+  random: 2
+}
